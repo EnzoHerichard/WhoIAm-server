07d74cd6bf27fd756bf3c20967462a80fe7773fd
EnzoHerichard <herichardenzo@gmail.com>
2024-06-23 19:48:58 +0200

    feat: try routes and get all celebrities name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, g
+from flask import Flask, g, jsonify, request
 from flask_cors import CORS
 
 import sqlite3
@@ -20,6 +20,41 @@ def close_connection(exception):
     db = g.pop('db', None)
     if db is not None:
         db.close()
+
+@app.route("/try", methods=["GET"])
+def try_route():
+    name = request.args.get('name')
+    if not name:
+        return jsonify({"error": "Name parameter is required"}), 400
+
+    cur = get_db().cursor()
+    cur.execute('SELECT * FROM celebrities WHERE name = ?', (name,))
+    celebrity = cur.fetchone()
+    
+    if celebrity is None:
+        return jsonify({"error": "Celebrity not found"}), 404
+    
+    age = get_age_of_celebrity(celebrity[2])
+    data = {
+        'name': celebrity[1],
+        'age': age,
+        'gender': celebrity[3],
+        'nationality': celebrity[4],
+        'occupation': celebrity[5]
+    }
+    
+    return jsonify(data)
+
+@app.route("/celebrities")
+def all():
+    cur = get_db().cursor()
+    cur.execute('SELECT name FROM celebrities')
+    celebrities = cur.fetchall()
+    data = []
+    for celebrity in celebrities:
+        data.append(celebrity[0])
+        
+    return jsonify(data)
 
 @app.route("/random")
 def random():
